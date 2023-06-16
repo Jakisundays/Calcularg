3622acafa8ebf8cd7f5cbab27bf6823aa50015f6
Jakisundays <jakisundays@gmail.com>
2023-06-16 18:01:04 -0300

    fixed scroll and inflacion states

--- client/src/hooks/useInflacion.js
+++ client/src/hooks/useInflacion.js
@@ -11,11 +11,12 @@ const useInflacion = () => {
         const responseMensual = await fetch(`${process.env.REACT_APP_SERVER_URL}/api/brca/inflacion_mensual`);
         const dataMensual = await responseMensual.json();
         // console.log(dataMensual);
-        setInflacionMensual(dataMensual);
-
+        
         const responseAnual = await fetch(`${process.env.REACT_APP_SERVER_URL}/api/brca/inflacion_anual`);
         const dataAnual = await responseAnual.json();
         // console.log(dataAnual);
+        
+        setInflacionMensual(dataMensual);
         setInflacionAnual(dataAnual);
       } catch (error) {
         console.log("Error al obtener los datos de inflación:", { error });
