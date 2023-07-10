641d7e2b00b0dfc41ef8071caf326679f7a54506
Jakisundays <jakisundays@gmail.com>
2023-07-10 19:36:23 -0300

    dolares added fixed

--- client/src/layouts/Dolargento/index.js
+++ client/src/layouts/Dolargento/index.js
@@ -41,7 +41,7 @@ const Dolargento = () => {
         ];
         setReales(reales);
       } catch (error) {
-        // console.log({ error });
+        console.log({ error });
         setError(true);
       }
     };
